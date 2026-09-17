8f1ef76c43c7bb1391e73826335f5e25bdf8ab02
Shreemabrata Patra <148060475+shreema4u@users.noreply.github.com>
2026-09-17 21:04:10 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,3 +1,3 @@
 def call(String ProjectName,String imageTag,String DockerHubUser){
-  sh "docker build -t $(DockerHubUser)/$(ProjectName):$(imageTag) ."
+  sh "docker build -t $(ProjectName):$(imageTag) ."
 }
